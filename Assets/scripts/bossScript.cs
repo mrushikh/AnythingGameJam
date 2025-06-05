759587a9271663afa24b722a672f3aba99103fd2
maxshatalovaudio <maxshatalovaudio@gmail.com>
2025-06-05 10:09:18 +0300

    Implemented dynamic music based on boss health

--- Assets/scripts/bossScript.cs
+++ Assets/scripts/bossScript.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 using UnityEngine.UI;
+using FMODUnity;
+using FMOD.Studio;
 
 public class bossScript : MonoBehaviour
 {
     public int health;
     public Slider Slider2;
     private float maxVal;
+    private StudioEventEmitter emitter;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         maxVal = health;
+        emitter = GetComponent<StudioEventEmitter>();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -32,5 +36,20 @@ public class bossScript : MonoBehaviour
 
         healthbar(health,maxVal);
 
+         if (emitter != null && emitter.IsPlaying())
+        {
+            if (health <= maxVal / 3f)
+            {
+                emitter.SetParameter("BossPhase", 3);
+            }
+            else if (health <= (2f * maxVal) / 3f)
+            {
+                emitter.SetParameter("BossPhase", 2);
+            }
+            else
+            {
+                emitter.SetParameter("BossPhase", 1);
+            }
+        }
     }
 }
